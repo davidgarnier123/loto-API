fe872b9586f30a2559c43188b62cd18a2f446f14
Da <Da@Da.com>
2023-01-06 17:01:20 +0100

    change cors rule

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.use(bodyParser.json());
 
 
 app.all('*', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     if ('OPTIONS' == req.method) {
     res.sendStatus(200);
